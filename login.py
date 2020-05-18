a13e76efc8e06978d0259f601aba6a8e30943f51
OtienoSamwel <otienosamwel135@gmail.com>
2020-05-18 16:22:38 +0300

    slight changes

--- login.py
+++ login.py
@@ -3,12 +3,12 @@ from tkinter import*
 def main_screen():
     screen = Tk()
     screen.geometry("720x1440")
-    screen.title("Notes 1.01")
-    Label(text = "Notes ", bg = "grey", width = "300", height = "2",font = ("calibri", 13)).pack()
+    screen.title("SAMPLE LOGIN PAGE")
+    Label(text = "LOG IN SCREEN ", bg = "grey", width = "300", height = "2",font = ("calibri", 13)).pack()
     Label(text = "").pack()
-    Button(text = "Login",height = "2", width = "30",bg = "green").pack()
+    Button(text = "LOGIN",height = "2", width = "30",bg = "green").pack()
     Label(text = "").pack()
-    Button(text = "Register", height= "2", width = "30", bg = "blue").pack()
+    Button(text = "REGISTER", height= "2", width = "30", bg = "blue").pack()
 
     screen.mainloop()
 
