a7dfd4c09ffd0902de54affed74d352d089690bc
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-18 16:06:18 +0300

    Added functionality to register button

--- login.py
+++ login.py
@@ -1,14 +1,14 @@
 from tkinter import*
+from Registration.register import *
 
 def main_screen():
-    screen = Tk()
     screen.geometry("720x1440")
     screen.title("SAMPLE LOGIN PAGE")
     Label(text = "LOG IN SCREEN ", bg = "grey", width = "300", height = "2",font = ("calibri", 13)).pack()
     Label(text = "").pack()
     Button(text = "LOGIN",height = "2", width = "30",bg = "green").pack()
     Label(text = "").pack()
-    Button(text = "REGISTER", height= "2", width = "30", bg = "blue").pack()
+    Button(text = "REGISTER", height= "2", width = "30", bg = "blue", command = register).pack()
 
     screen.mainloop()
 
