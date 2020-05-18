ce83c2fdedd8cedd52273006c5bf25121eef2dfa
OtienoSamwel <otienosamwel135@gmail.com>
2020-05-18 18:18:14 +0300

    slight changes

--- Registration/register.py
+++ Registration/register.py
@@ -14,13 +14,18 @@ def register():
     Label(reg_screen, text = "Enter the details below", bg = "blue").pack()
     Label(reg_screen, text = "").pack()
 
-    username_label = Label(reg_screen, text = "Username ")
+    username_label = Label(reg_screen, text = "ENTER USERNAME")
     username_label.pack()
 
     username_entry = Entry(reg_screen, textvariable = username)
     username_entry.pack()
 
+    Label(text ="").pack()
+    Label(reg_screen,text ="ENTER PASSWORD").pack()
+
     password_entry = Entry(reg_screen, textvariable = password, show = '*')
     password_entry.pack()
 
-    Button(reg_screen, text = "Register", width = 10, height = 1, bg = "blue").pack()
+    Label(reg_screen,text ="").pack()
+    Button(reg_screen, text = "REGISTER", width = 10, height = 1, bg = "blue").pack()
+    
