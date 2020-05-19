259c92ff6c96162d5c96526f6ddd7daade8da115
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-19 13:40:21 +0300

    Added comments

--- login.py
+++ login.py
@@ -1,15 +1,21 @@
 from tkinter import*
 from Registration.register import *
 
+#This is the main login screen function
 def main_screen():
-    screen.geometry("720x1440")
+    screen.geometry("720x1440") #Screen resolution
     screen.title("SAMPLE LOGIN PAGE")
     Label(text = "LOG IN SCREEN ", bg = "grey", width = "300", height = "2",font = ("calibri", 13)).pack()
     Label(text = "").pack()
+
+    #The Login button
     Button(text = "LOGIN",height = "2", width = "30",bg = "green").pack()
     Label(text = "").pack()
+
+    #The Register button
     Button(text = "REGISTER", height= "2", width = "30", bg = "blue", command = register).pack()
 
     screen.mainloop()
 
+#Call the function to run infinitely
 main_screen()
